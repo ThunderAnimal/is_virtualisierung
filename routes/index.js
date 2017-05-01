5e8824f6a459a98bdc6cc9108536f1bda9850b4c
ThunderAnimal <MartinWeber.93@outlook.com>
2017-05-01 11:44:52 +0200

    - fiil dummy structur - refactor layout - adds impressum page - improves design error page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'IS Virtualisierung' });
+});
+
+/* Get Impressum Page */
+router.get('/impressum', function(req, res, next) {
+  res.render('impressum', { title: 'Impressum - IS Virtualisierung' });
 });
 
 module.exports = router;
