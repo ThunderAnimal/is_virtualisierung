1af81268d01d40334686b5f731796de618b50ed0
Martin Weber <mweber@hh-berlin.de>
2017-05-08 19:21:03 +0200

    improve design map

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,16 @@
  	
  	$(".button-collapse").sideNav();
 
-    $('ul.tabs').tabs({ 'swipeable': true });
-  });
+  $('ul.tabs').tabs({ 'swipeable': true , heightStyle: "fill" });
+
+  $('#googleMap').height($( window ).height() - 112);
+  
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&'+'callback=initialize';
+  document.body.appendChild(script);
+
+});
 
   function loadKey() {
       var script = document.createElement('script');
@@ -14,23 +22,8 @@
       }
   function initialize() {
       var map = new google.maps.Map(document.getElementById('googleMap'), {
-      center: {lat: 52.520008, lng: 13.404954},
-      scrollwheel: false,
-      zoom: 8
-      });
-
-
+        center: {lat: 52.520008, lng: 13.404954},
+        scrollwheel: true,
+        zoom: 12
+        });
       }
-
-      function loadScript() {
-	  document.getElementById('googleMap').style.height="400px";
-	   document.getElementById('googleMap').style.marginLeft="10px";
-	    document.getElementById('googleMap').style.marginRight="10px";
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&'+'callback=initialize';
-      document.body.appendChild(script);
-
-      }
-
-      window.onload= loadScript;
