cdba0b188faf5b24d62a2fd469369e5fcb4d4d83
appolin <kemouj@yahoo.fr>
2017-05-12 16:59:23 +0200

    public_js_main.js

--- public/js/main.js
+++ public/js/main.js
@@ -15,6 +15,9 @@
   $('#filterContainer').height($( window ).height() - 112);
   $('.tabs-content').height($( window ).height() - 112);
 
+ 
+  
+
 });
 
   function loadKey() {
@@ -24,10 +27,50 @@
       document.body.appendChild(script);
 
       }
+	  
   function initialize() {
-      var map = new google.maps.Map(document.getElementById('googleMap'), {
+     var map = new google.maps.Map(document.getElementById('googleMap'), {
         center: {lat: 52.520008, lng: 13.404954},
         scrollwheel: true,
         zoom: 12
         });
+		
+		
+		var count = 0;
+		var positionList = new Array("52.520000;13.404950", "52.510000;13.414950", "52.500000;13.404950");
+		//die Marker wurde online unter http://earth.google.com/images/kml-icons/track-directional/track-0.png genommen
+		while(count < positionList.length ){
+		  
+		  var lat=parseFloat(positionList[count].split(';')[0]).toFixed(7);
+		   var long=parseFloat(positionList[count].split(';')[1]).toFixed(7);
+		   
+		 var marker = new google.maps.Marker({
+          position:  new google.maps.LatLng(lat, long),
+           icon:'http://earth.google.com/images/kml-icons/track-directional/track-0.png',
+          map: map
+        });
+		count ++;
+		}
+
+
+		//einfach position markieren:  ohne coustom marker
+	  
+		/*while(count < positionList.length ){  
+		  var lat=parseFloat(positionList[count].split(';')[0]).toFixed(7);
+		   var long=parseFloat(positionList[count].split(';')[1]).toFixed(7);
+		 var marker = new google.maps.Marker({
+          position:  new google.maps.LatLng(lat, long),
+          map: map
+        });
+		count ++;
+		}*/
+
+      
+	  
+	  
+
+
       }
+
+
+	   
