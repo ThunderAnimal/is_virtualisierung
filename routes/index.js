0dd73d0d3b9e5899377a4f631e95a6fc421f784e
ThunderAnimal <MartinWeber.93@outlook.com>
2017-05-18 20:45:06 +0200

    fix titles

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IS Virtualisierung' });
+  res.render('index', { title: 'IS Visualisierung' });
 });
 
 /* Get Impressum Page */
 router.get('/impressum', function(req, res, next) {
-  res.render('impressum', { title: 'Impressum - IS Virtualisierung' });
+  res.render('impressum', { title: 'Impressum - IS Visualisierung' });
 });
 
 module.exports = router;
