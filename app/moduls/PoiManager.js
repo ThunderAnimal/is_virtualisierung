08569d09de3ef26ff6f124eda032e14ac514c0d0
AnnaMyak <myakinen@gmail.com>
2017-06-22 21:08:45 +0200

    Centroid calculation

--- app/moduls/PoiManager.js
+++ app/moduls/PoiManager.js
@@ -68,37 +68,79 @@ exports.deleteDataWithputName = function (callback) {
 function getCoords(objGeo, objPois) {
     var all = new Array();
     var len = objPois.length;
+	
+	
+	
+	
     for (i in  objPois)
     {
-        var streetPoi=  JSON.stringify(objPois[i].adressen);
-        var name=  JSON.stringify(objPois[i].name);
-        streetPoi=streetPoi.split(':')[0];
-        streetPoi= streetPoi.replace('{',"");
-        streetPoi= streetPoi.replace(/\s/g,'');
-        streetPoi = streetPoi.substr(1, streetPoi.length-2);
-
-        name=name.split(':')[0];
+		var name=  JSON.stringify(objPois[i].name);
+		
+		name=name.split(':')[0];
         name= name.replace('{',"");
         name= name.replace('}',"");
         name= name.replace('[',"");
         name= name.replace(']',"");
         name= name.replace(/\s/g,'');
         name = name.substr(1, name.length-2);
-        //console.log(name);
         var coords = new Array();
+		
+		if (JSON.stringify(objPois[i].adressen).split(':').length>1)
+		{
+				var poi = new Array();
+				var centroidCoords= calcCentroid(JSON.stringify(objPois[i].adressen));
+				//console.log("longi "+test.longitude);
+				var id = JSON.stringify(objPois[i].id);
+                id = id.substring(1,id.length-1);
+                var typ = JSON.stringify(objPois[i].typ);
+                typ = typ.substring(1,typ.length-1);
+                var lat = centroidCoords.latitude;
+                var lon = centroidCoords.longitude;
+                poi.push(id),poi.push(typ);poi.push(name);poi.push(lat); poi.push(lon);
+                if (poi[0]!=null &&poi[1]!=null&& poi[2]!=null &&poi[3]!=null&& poi[4]!=null && coords.length==0)
+                {
+                    coords.push(poi);
+                    all.push (poi);
+
+                }
+
+                if (coords.length==1)
+                {
+                    //console.log(coords[0]);
+					//console.log("added");
+                    addToDb(coords[0]);
+                }
+			
+		}
+		else (JSON.stringify(objPois[i].adressen).split(':').length==1)
+		{
+			        var streetPoi=  JSON.stringify(objPois[i].adressen);
+        
+        streetPoi=streetPoi.split(':')[0];
+        streetPoi= streetPoi.replace('{',"");
+        streetPoi= streetPoi.replace(/\s/g,'');
+        streetPoi = streetPoi.substr(1, streetPoi.length-2);
+
+      
+		
+	
         for (j in objGeo)
         {
+			
             var street=  JSON.stringify(objGeo[j].street);
             street=street.split(':')[0];
             street= street.replace('{',"");
             street= street.replace(/\s/g,'');
             street = street.substr(1, street.length-2);
             var poi = new Array();
-
-
+			
+			
 
             if (street==streetPoi &&  coords.length==0)
             {
+				
+				
+				
                 var id = JSON.stringify(objPois[i].id);
                 id = id.substring(1,id.length-1);
                 var typ = JSON.stringify(objPois[i].typ);
@@ -118,16 +160,77 @@ function getCoords(objGeo, objPois) {
                 if (coords.length>0)
                 {
                     //console.log(coords[0]);
+					//console.log("added");
                     addToDb(coords[0]);
                 }
-            }
+				}
 
 
 
 
-        }
+			}
+		}
+
     }
-    console.log("All "+ all.length);
+    //console.log("All "+ all.length);
+}
+
+function calcCentroid(addresses)
+{
+		var addr=addresses.split(":");
+		var sumLon=0;
+		var sumLat=0;
+		var centroidCoords= new Array();
+		for(k in addr)
+		{
+			addr[k]=addr[k].replace('{',"");
+			addr[k]=addr[k].replace('}',"");
+			addr[k]=addr[k].replace('[',"");
+			addr[k]=addr[k].replace(']',"");
+			addr[k]=addr[k].replace(/,/g , "");
+			addr[k]=addr[k].replace(/\s/g,'');;
+			addr[k]=addr[k].replace('.',"");
+			addr[k]=addr[k].replace(/[0-9]/g, '');
+			addr[k]=addr[k].substr(1,addr[k].length-2);
+			//console.log(addr[k]);
+			for (j in contentGeo)
+			{
+					
+					var street=  JSON.stringify(contentGeo[j].street);
+					street=street.split(':')[0];
+					street= street.replace('{',"");
+					street= street.replace(/\s/g,'');
+					street = street.substr(1, street.length-2);
+					var poi = new Array();
+					if(addr[k]==street)
+					{
+						var lat = JSON.stringify(contentGeo[j].lat);
+						lat = lat.substring(1,lat.length-1);
+						var lon = JSON.stringify(contentGeo[j].lon);
+						lon = lon.substring(1,lon.length-1);
+						//console.log("lon" + lon);
+						//console.log("lon" + lat);
+						poi.push(lon);poi.push(lat);
+						centroidCoords.push(poi);
+					}
+			
+						
+			}
+		}
+		//console.log("Centroid " + centroidCoords.length);
+		if (centroidCoords.length>0)
+		{
+			
+			for(c in centroidCoords)
+			{
+				sumLon= sumLon + parseFloat(centroidCoords[c][0]);
+				sumLat= sumLat + parseFloat(centroidCoords[c][1]);
+			}
+			
+		}
+		//console.log("lon " + sumLon/centroidCoords.length);
+		//console.log("lat " + sumLat/centroidCoords.length);
+		return {latitude: sumLat/centroidCoords.length, longitude:sumLon/centroidCoords.length};
 }
 
 function addToDb(poiObject) {
@@ -139,3 +242,5 @@ function addToDb(poiObject) {
         console.log("Denkmal Datenbank erfolgreich gefuellt");
     }).catch(dbHelper.onError);
 }
+
+
