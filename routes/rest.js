242c2da42ba23762f9f3d763e9b05bb64ffeb8ef
ThunderAnimal <MartinWeber.93@outlook.com>
2017-06-25 21:18:26 +0200

    implemtes rest api

--- routes/rest.js
+++ routes/rest.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var router = express.Router();
 
+var ereignisManager = require('../app/moduls/EreignisManager');
+var poiManager = require('../app/moduls/PoiManager');
 var dataManager = require('../app/dataManager');
 
 /* REST API  */
@@ -14,4 +16,20 @@ router.get('/filldata', function(req, res, next) {
     });
 });
 
+router.get('/markers', function (req, res) {
+    dataManager.queryMarkers(req.query, function (markers) {
+        res.send({markers: markers});
+    });
+});
+
+router.get('/ereignis/:id', function (req, res) {
+    ereignisManager.getItemContent(req.params.id, function(data) {
+        res.send(data);
+    });
+});
+router.get('/denkmal/:id', function (req, res) {
+    poiManager.getItemContent(req.params.id, function (data) {
+       res.send(data);
+    });
+});
 module.exports = router;
